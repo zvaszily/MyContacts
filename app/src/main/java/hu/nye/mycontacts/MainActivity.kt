7d9f09a10e7ffdcc93bf8d67f063f58301ab5f48
Zsolt Vaszily <zvaszily@gmail.com>
2022-11-23 11:47:34 +0100

    create toolbar icons

--- app/src/main/java/hu/nye/mycontacts/MainActivity.kt
+++ app/src/main/java/hu/nye/mycontacts/MainActivity.kt
@@ -2,10 +2,27 @@ package hu.nye.mycontacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        setSupportActionBar(findViewById(R.id.contact_list_toolbar))
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            title = getString(R.string.codeyard)
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
